48c242ed9f9a293c0b9d9b7a693d536adf3baf48
saikiran <obscuresai@gmail.com>
2024-02-19 17:52:13 +0530

    variables

--- Conditions(26)/ec2.tf
+++ Conditions(26)/ec2.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "web" {   
   ami           =  var.ami_id
-  instance_type = var.instance_type == mongodb ? "t3.medium" : "t2.micro"
+  instance_type = var.instance_type == "mongodb" ? "t3.medium" : "t2.micro"
 }
