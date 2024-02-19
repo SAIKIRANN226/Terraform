410403442531adaa16b207d31ef283a0fcad9e34
saikiran <obscuresai@gmail.com>
2024-02-19 19:13:22 +0530

    variables

--- Locals(26)/locals.tf
+++ Locals(26)/locals.tf
@@ -1,4 +1,4 @@
 locals {
-    instance_type = var.instance-names[count.index] == "mongodb" || var.instance-names[count.index] == "mysql" ? "t3.small" : "t2.micro"
-    ip = [var.instance-names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
+  instance_type = var.instance-names[count.index] == "mongodb" || var.instance-names[count.index] == "mysql" ? "t3.small" : "t2.micro"
+  ip = [var.instance-names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
 }
