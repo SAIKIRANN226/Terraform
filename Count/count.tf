8a9f19a9585b2a32baf045f95ebbf5d27acf1623
saikiran <obscuresai@gmail.com>
2024-02-19 18:58:56 +0530

    variables

--- Count/count.tf
+++ Count/count.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "web" {   
-  count = 5 # count.index is a special variable
+  #count = 5 # count.index is a special variable
+  count = length(var.instance-names)  # you may forget to update the count number so we use length
   ami           =  var.ami_id
   instance_type = var.instance-names[count.index] == "mongodb" || var.instance-names[count.index] == "mysql" ? "t3.small" : "t2.micro"
   tags = {
@@ -8,7 +9,8 @@ resource "aws_instance" "web" {
 }
 
 resource "aws_route53_record" "www" {
-  count = 5
+  #count = 5
+  count = length(var.instance-names)
   zone_id = var.zone_id
   name    = "${var.instance-names[count.index]}.${var.domain_name}"   # mixing variable and static text = interpolation
   type    = "A"
