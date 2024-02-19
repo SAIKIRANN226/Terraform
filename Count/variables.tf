c5dc836f685471ab4fb1adf84c70ed626c5e0044
saikiran <obscuresai@gmail.com>
2024-02-19 18:45:54 +0530

    variables

--- Count/variables.tf
+++ Count/variables.tf
@@ -1,8 +1,16 @@
-variable "instance_type" {
-    type = string
-    default = "mongodb"
+variable "instance-names" {
+    type = list
+    default = ["web","cart","catalogue","shipping","mysql","payment"]
 }
 variable "ami_id" {
     type = string
     default = "ami-0f3c7d07486cad139"
+}
+variable "zone_id" {
+    type = string
+    default = "Z05104061G7J749VWUD3O"
+}
+variable "domain_name" {
+    type = string
+    default = "saidevops.fun"
 }
