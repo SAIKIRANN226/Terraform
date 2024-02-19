d2c6ad6b10ad0b47aa9ba0e3f3842c5b7e0f6b2d
saikiran <obscuresai@gmail.com>
2024-02-19 18:28:05 +0530

    variables

--- Count/count.tf
+++ Count/count.tf
@@ -1,8 +1,17 @@
 resource "aws_instance" "web" {   
   count = 5 # count.index is a special variable
   ami           =  var.ami_id
-  instance_type = "t2.micro"
+  instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == mysql ? "t3.small" : "t2.micro"
   tags = {
     Name = var.instance_names[count.index]
   }
+}
+
+resource "aws_route53_record" "www" {
+  count = 5
+  zone_id = var.zone_id
+  name    = "${var.instance_names[count.index]}.${var.domain_name}"   # mixing variable and static text = interpolation
+  type    = "A"
+  ttl     = 300
+  records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
 }
