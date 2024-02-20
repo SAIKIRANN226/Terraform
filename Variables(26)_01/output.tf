d073d165a5a586d38fdcec691d38eaf918b5915d
saikiran <obscuresai@gmail.com>
2024-02-20 16:41:12 +0530

    variables

---
+++ Variables(26)_01/output.tf
@@ -0,0 +1,15 @@
+# output "instance_info" {
+#    value = aws_instance.web
+# }  #commented because it provide huge information
+
+# output "instance_id" {
+#    value = aws_instance.web.id
+# }
+# output "public_ip" {
+#    value = aws_instance.web.public_ip
+# }
+# output "private_ip" {
+#    value = aws_instance.web.private_ip
+# }
+
+# #we can get the information from the ouput resources
