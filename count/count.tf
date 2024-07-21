f168bdf8533bb587852f1712eb0f6e321a0d0286
mothukuru saikiran <obscuresai.com>
2024-07-21 12:45:45 +0530

    message

--- count/count.tf
+++ count/count.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   #count = 11 # count.index is a special variable given by terraform,because it was creating instances with same names for that we use count.index
-  count = length(var.instance_names)
+  count = length(var.instance_names)  # length is the function given by terraform
   ami           = var.ami_id #devops-practice
   instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql" || var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
   tags = {
