7515dba3b73f10e1d5d72077b33f1cbae1af355e
mothukuru saikiran <obscuresai.com>
2024-07-21 12:19:57 +0530

    message

--- variables/output.tf
+++ variables/output.tf
@@ -1,6 +1,16 @@
-# output "instance_info" {
+# attribute refrence it will give all the information,output block is used to print the output of the resources in the terminal just terraform plan
+
+# output "instance_info" {      
 #   value = aws_instance.web
 # }
+# the above one by terraform apply you will get all info
+
+
+
+
+
+
+# if you want any particular information like the below ones
 
 output "instance_id" {
   value = aws_instance.web.id
