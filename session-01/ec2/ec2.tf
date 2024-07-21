0fb988ac8935e392749706283dfae7dd0abe1283
mothukuru saikiran <obscuresai.com>
2024-07-21 11:01:09 +0530

    message

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb" #devops-practice
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this means list and this line taken from the line 11
 
   tags = {
     Name = "HelloTerraform"
