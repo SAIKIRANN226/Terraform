dccde959699e499c48609e368f2c625660e58ad2
mothukuru saikiran <obscuresai.com>
2024-07-23 09:33:32 +0530

    message

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-03265a0778a880afb" #devops-practice
+  ami           = "ami-0b4f379183e5706b9" #devops-practice
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.roboshop-all.id] # this [ ] means list and this line taken from the line 11,terraform will automatically takecare of the dependencies,you can see here firt it will create sg and then ec2,no need to follow the sequence also, and this is also a output of security group beacsue we used .id to get output
   
