c1cfecde5eb3bb1a8e6fa076de3c35c7e3b8298d
mothukuru saikiran <obscuresai.com>
2024-08-30 12:37:43 +0530

    message

--- foreach/foreach.tf
+++ foreach/foreach.tf
@@ -7,15 +7,6 @@ resource "aws_instance" "web" {
   }
 }
 
-# resource "aws_route53_record" "www" {
-#   #count = 11
-#   count = length(var.instance_names)
-#   zone_id = var.zone_id
-#   name    = "${var.instance_names[count.index]}.${var.domain_name}" #interpolation
-#   type    = "A"
-#   ttl     = 1
-#   records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
-# }
 
 resource "aws_route53_record" "www" {
   for_each = aws_instance.web
