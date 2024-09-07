e99a16b3ca4b0cec938dbcc96af9265ac00f6ee8
mothukuru saikiran <obscuresai.com>
2024-09-07 09:55:26 +0530

    message

--- count/count.tf
+++ count/count.tf
@@ -12,7 +12,7 @@ resource "aws_route53_record" "www" {
   #count = 11
   count = length(var.instance_names)
   zone_id = var.zone_id # zone id can found in the hosted zones
-  name    = "${var.instance_names[count.index]}.${var.domain_name}" #interpolation(mixing variable + static text) before this delete old records if exists
+  name    = "${var.instance_names[count.index]}.${var.domain_name}" #interpolation(mixing variable + static text)before this delete old records if exists
   type    = "A"
   ttl     = 1
   records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
