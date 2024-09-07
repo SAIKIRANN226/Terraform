dd1ffe72d8680b0eb263c1fb95b0ee8193358db6
mothukuru saikiran <obscuresai.com>
2024-09-07 10:00:50 +0530

    message

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.31.0" # AWS provider version, not terraform version
+      version = "5.31.0"
     }
   }
 
   backend "s3" {
     bucket = "daws76s-remote-state"
-    key    = "foreach"  # your bucket information will be saved here in foreach
+    key    = "foreach"  # your bucket information will be saved here in foreach key name this is our wish we can create as per our requirement
     region = "us-east-1"
     dynamodb_table = "daws76s-locking"  # table name you are using to lock the bucket,here you need to reconfigure by "terraform init -reconfigure"
   }
