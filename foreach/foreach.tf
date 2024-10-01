eab03d1a09be067adfe336a194ad8187c1bba3a5
mothukuru saikiran <obscuresai.com>
2024-10-01 13:47:57 +0530

    message

--- foreach/foreach.tf
+++ foreach/foreach.tf
@@ -14,7 +14,7 @@ resource "aws_route53_record" "www" {
   name    = "${each.key}.${var.domain_name}" #interpolation
   type    = "A"
   ttl     = 1
-  records = [each.key == "web" ? each.value.public_ip : each.value.private_ip ]
+  records = [each.key == "web" ? each.value.public_ip : each.value.private_ip]
 }
 
 # output "instances_info" {
