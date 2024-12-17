b8fabb6c33a6f735ae71b06e449672bc0b67aa21
mothukuru saikiran <obscuresai.com>
2024-12-17 12:56:54 +0530

    message

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "web" {
   ami           = "ami-0b4f379183e5706b9" # devops-practice
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.roboshop-all.id] # This [ ] means list and this line taken from the line 11,terraform will automatically takecare of the dependencies,you can see here first it will create sg and then ec2,no need to follow the sequence also, and this is also a output of security group because we used .id to get output; line 4 is only used when there is a security group is created
-  
   tags = {
     Name = "HelloTerraform"
     Environemnt = "dev"
@@ -16,7 +15,6 @@ resource "aws_security_group" "roboshop-all" { # This is terraform name, for ter
     name        = var.sg-name # This is for AWS
     description = var.sg-description
     
-
     ingress {
         description      = "Allow All ports"
         from_port        = var.inbound-from-port # 0 means all ports
