340186b331f16bdbbe5735afef10b9cad59dab14
mothukuru saikiran <obscuresai.com>
2024-12-17 12:53:39 +0530

    message

--- session-01/ec2/ec2.tf
+++ session-01/ec2/ec2.tf
@@ -1,14 +1,14 @@
 resource "aws_instance" "web" {
   ami           = "ami-0b4f379183e5706b9" # devops-practice
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.roboshop-all.id] # This [ ] means list and this line taken from the line 11,terraform will automatically takecare of the dependencies,you can see here firt it will create sg and then ec2,no need to follow the sequence also, and this is also a output of security group beacsue we used .id to get output; line 4 is only used when there is a security group is created
+  vpc_security_group_ids = [aws_security_group.roboshop-all.id] # This [ ] means list and this line taken from the line 11,terraform will automatically takecare of the dependencies,you can see here first it will create sg and then ec2,no need to follow the sequence also, and this is also a output of security group because we used .id to get output; line 4 is only used when there is a security group is created
   
   tags = {
     Name = "HelloTerraform"
     Environemnt = "dev"
     Component = "web"
     Terraform = "true"
-    Project = "roboshop" # That means this instance is created through terraform
+    Project = "roboshop" # That means this instance is created using terraform
   }
 }
 
