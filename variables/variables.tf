647a57eee48562f7f5587f32410835080944fb1c
mothukuru saikiran <obscuresai.com>
2025-04-16 18:04:25 +0530

    terraform

--- variables/variables.tf
+++ variables/variables.tf
@@ -2,7 +2,7 @@
 # 2. -var-file
 # 3. terraform.tfvars
 # 4. ENV variables
-# if you dont find in any of the above then terraform will prompt for the value
+# If you dont find in any of the above then terraform will prompt for the value
 
 variable "ami_id" {
   type = string
@@ -15,19 +15,19 @@ variable "instance_type" {
 }
 
 variable "tags" {
-  type = map       # If default is starting from the flower brases then it is map
+  type = map       # If default is starting from the flower brases then it is a map, this is the way to declare a map
   default = {
     Name = "Hello Terraform"
     Project = "Roboshop"
     Environment = "DEV"
     Component = "Web"
-    Terraform = "true"  # means this ec2 is created using terraform
+    Terraform = "true"  # Means this ec2 is created using terraform
   }
 }
 
 variable "sg-name" {
   type = string
-  default = "roboshop-all"  # if it string then default value must be in double qotes ""
+  default = "roboshop-all"  # If it is string then default value must be in double qotes ""
 }
 
 variable "sg-description" {
@@ -37,10 +37,10 @@ variable "sg-description" {
 
 variable "inbound-from-port" {
     type = number
-    default = 0            # if it is number then default value just number no need to put in ""
+    default = 0            # If it is a number then default value just number no need to put in ""
 }
 
 variable "cidr_blocks" {
   type = list
-  default = ["0.0.0.0/0"]  # if it is list must be in [] inside it must be in qotes (or) CIDR blocks should be in list[] and inside it must in double qotes
+  default = ["0.0.0.0/0"]  # If it is list must be in [] inside it must be in qotes (or) CIDR blocks should be in list[] and inside it must in double qotes
 }
