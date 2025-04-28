dd51716a2d12b3c25112222b6dc39be818315053
mothukuru saikiran <obscuresai.com>
2025-04-28 15:42:08 +0530

    terraform

--- count/count.tf
+++ count/count.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  #count = 11 --->count.index is a special variable given by terraform,because it was creating instances with same names for that we use count.index,why we are not using count then? because here also sometimes we foget to give the correct number so thats why we use length function given by terraform
+  #count = 11 ---> count.index is a special variable given by terraform, because it was creating instances with same names for that we use count.index, why we are not using count then ? because here also sometimes we forget to give the correct number so thats why we use length function given by terraform
   count = length(var.instance_names)  
   ami           = var.ami_id
   instance_type = var.instance_names[count.index] == "mongodb" || var.instance_names[count.index] == "mysql" || var.instance_names[count.index] == "shipping" ? "t3.small" : "t2.micro"
@@ -11,8 +11,8 @@ resource "aws_instance" "web" {
 resource "aws_route53_record" "www" {
   #count = 11
   count = length(var.instance_names)
-  zone_id = var.zone_id # zone id can found in the hosted zones
-  name    = "${var.instance_names[count.index]}.${var.domain_name}" # interpolation(mixing variable + static text)before this delete old records if exists
+  zone_id = var.zone_id # Zone id can found in the hosted zones
+  name    = "${var.instance_names[count.index]}.${var.domain_name}" # Interpolation(mixing variable + static text)before this delete old records if exists
   type    = "A"
   ttl     = 1
   records = [var.instance_names[count.index] == "web" ? aws_instance.web[count.index].public_ip : aws_instance.web[count.index].private_ip]
