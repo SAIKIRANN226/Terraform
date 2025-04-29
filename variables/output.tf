2a578be8b1a3fdf737bed9ee14cdd7b206a5e3b2
mothukuru saikiran <obscuresai.com>
2025-04-29 16:08:11 +0530

    terraform

--- variables/output.tf
+++ variables/output.tf
@@ -1,18 +1,15 @@
-# attribute reference ---> it will give all the information,output block is used to print the output of the resources in the terminal just terraform plan
+# Attribute reference ---> It will give all the information,output block is used to print the output of the resources in the terminal just terraform plan
 
 # where this output will be useful? is used to create another resource by giving output of one instance to another resource as input (OR) one ouput can be given as input to the other
 
 # output "instance_info" {      
 #   value = aws_instance.web
 # }
-# the above one by terraform apply you will get complete information on the terminal thats why we commented this
+# The above one by terraform apply you will get complete information on the terminal thats why we commented this
 
 
 
-
-
-
-# if you want any particular information like the below ones
+# If you want any particular information like the below ones
 
 output "instance_id" {
   value = aws_instance.web.id
